a40726b77e79bcc6922b1c9bf221f8ccaf0c064d
Maksim Kostromin <daggerok@gmail.com>
2018-02-22 06:12:25 +0200

    simple spring-cloud-functions example

--- spring-cloud-functions/src/main/java/daggerok/App.kt
+++ spring-cloud-functions/src/main/java/daggerok/App.kt
@@ -2,9 +2,21 @@ package daggerok
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.function.Function
 
 @SpringBootApplication
-class App
+class App {
+
+//  data class MyClass(val value: String = "")
+//  @Bean
+//  fun upMyClass(): Function<MyClass, MyClass> =
+//      Function { myClass -> MyClass(myClass.value.toUpperCase()) }
+
+  @Bean
+  fun up(): Function<Map<String, String>, Map<String, String>> =
+      Function { map -> map.mapValues { it.value.toUpperCase() } }
+}
 
 fun main(args: Array<String>) {
   runApplication<App>(*args)
