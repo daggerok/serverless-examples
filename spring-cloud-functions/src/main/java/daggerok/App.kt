889fea4ae16bb3b47cc7896712fa2f5f2cdafa42
Maksim Kostromin <daggerok@gmail.com>
2018-02-22 07:11:49 +0200

    remove useless shit again

--- spring-cloud-functions/src/main/java/daggerok/App.kt
+++ spring-cloud-functions/src/main/java/daggerok/App.kt
@@ -8,11 +8,6 @@ import java.util.function.Function
 @SpringBootApplication
 class App {
 
-//  data class MyClass(val value: String = "")
-//  @Bean
-//  fun upMyClass(): Function<MyClass, MyClass> =
-//      Function { myClass -> MyClass(myClass.value.toUpperCase()) }
-
   @Bean
   fun up(): Function<Map<String, String>, Map<String, String>> =
       Function { map -> map.mapValues { it.value.toUpperCase() } }
